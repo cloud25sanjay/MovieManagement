53f3627358ea907c028fb45d1d3ff5c5800b8d66
Sanjay <sanjay@gmail.com>
2026-07-28 19:46:14 +0530

    feat: add base entity

---
+++ src/MovieManagement.Domain/Common/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieManagement.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; protected set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void UpdateTimeStamps()
+        {
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+    }
+}
