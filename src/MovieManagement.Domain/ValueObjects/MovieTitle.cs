5aa376776546facc00a2ddf4f43f0a114c5de0ea
Sanjay <sanjay@gmail.com>
2026-07-28 20:04:27 +0530

    feat: add movie title value object

---
+++ src/MovieManagement.Domain/ValueObjects/MovieTitle.cs
@@ -0,0 +1,31 @@
+﻿using MovieManagement.Domain.Exceptions;
+
+
+namespace MovieManagement.Domain.ValueObjects
+{
+    public sealed class MovieTitle 
+    {
+        public string Value { get; private set; }
+
+        public MovieTitle(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new DomainException("Movie Title cannot be Empty");
+            }
+
+            if(value.Length > 200)
+            {
+                throw new DomainException("Movie Title cannot Exceed 200 characters");
+            }
+
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
+    }
+}
