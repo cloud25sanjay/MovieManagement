92ceb9913460770c2e4d067ea619ccc99abd1de9
Sanjay <sanjay@gmail.com>
2026-07-29 20:11:16 +0530

    feat: add create movie command handler

---
+++ src/MovieManagement.Application/Features/Movies/Commands/CreateMovie/CreateMovieCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieManagement.Application.Features.Movies.Commands.CreateMovie
+{
+    public sealed record CreateMovieCommand(string Title, string Synopsis, int Duration, DateTime ReleaseDate) : IRequest<Guid>;
+    
+}
---
+++ src/MovieManagement.Application/Features/Movies/Commands/CreateMovie/CreateMovieCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieManagement.Application.Features.Movies.Commands.CreateMovie
+{
+    public sealed class CreateMovieCommandHandler : IRequestHandler<CreateMovieCommand , Guid>
+    {
+        public async Task<Guid> Handle(CreateMovieCommand request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(Guid.NewGuid());
+        }
+    }
+}
