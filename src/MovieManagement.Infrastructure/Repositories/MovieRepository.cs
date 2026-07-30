5b52b54e8cc0fcceceb38a63f14f946d84f5ca11
Sanjay <sanjay@gmail.com>
2026-07-30 20:27:48 +0530

    feat: implement movie repository

---
+++ src/MovieManagement.Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieManagement.Application.Interfaces.Persistence;
+using MovieManagement.Domain.Entities;
+using MovieManagement.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieManagement.Infrastructure.Repositories
+{
+    public sealed class MovieRepository : IMovieRepository
+    {
+        private readonly MovieDbContext _dbContext;
+
+        public MovieRepository(MovieDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddASync(Movie movie, CancellationToken cancellationToken)
+        {
+            await _dbContext.Movies.AddAsync(movie, cancellationToken);
+        }
+
+        public async Task<Movie?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Movies.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+        }
+    }
+}
