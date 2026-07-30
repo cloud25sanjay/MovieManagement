c8370647b114c39ec584850b2f5613c491398b08
Sanjay <sanjay@gmail.com>
2026-07-30 20:54:12 +0530

    feat: add Infrastructure DependencyInjection

---
+++ src/MovieManagement.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MovieManagement.Application.Interfaces.Persistence;
+using MovieManagement.Infrastructure.Persistence;
+using MovieManagement.Infrastructure.Repositories;
+
+namespace MovieManagement.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<MovieDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<IMovieRepository, MovieRepository>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
