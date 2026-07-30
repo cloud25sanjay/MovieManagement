255d5cd071ff9fba3ec9a7c8e084f823f549da6b
Sanjay <sanjay@gmail.com>
2026-07-30 20:37:55 +0530

    feat: implement unit of work

---
+++ src/MovieManagement.Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using MovieManagement.Application.Interfaces.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieManagement.Infrastructure.Persistence
+{
+    public sealed class UnitOfWork : IUnitOfWork
+    {
+        private readonly MovieDbContext _dbContext;
+
+        public UnitOfWork(MovieDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
