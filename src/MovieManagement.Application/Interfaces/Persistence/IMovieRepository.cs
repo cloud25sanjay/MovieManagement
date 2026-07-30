52381aaf087ac4f6e7d80b7ef7bb03a54a380997
Sanjay <sanjay@gmail.com>
2026-07-30 19:33:12 +0530

    feat: add IMovieRepository Interface

---
+++ src/MovieManagement.Application/Interfaces/Persistence/IMovieRepository.cs
@@ -0,0 +1,13 @@
+﻿using MovieManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieManagement.Application.Interfaces.Persistence
+{
+    public interface IMovieRepository
+    {
+        Task AddASync(Movie movie, CancellationToken cancellationToken);
+        Task<Movie?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    }
+}
