c93b90717f63750417dc371371a2527530d53a54
Sanjay <sanjay@gmail.com>
2026-07-30 19:58:09 +0530

    feat: add MovieDbContext

---
+++ src/MovieManagement.Infrastructure/Persistence/MovieDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieManagement.Domain.Entities;
+
+
+namespace MovieManagement.Infrastructure.Persistence
+{
+    public sealed class MovieDbContext : DbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies => Set<Movie>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MovieDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
